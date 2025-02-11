99f86e8ec32bfec62e3a98498ab49a35162cc2d9
guinness1 <39463666-guinness1@users.noreply.replit.com>
2025-02-11 09:17:30 +0000

    Restored to 'edab637d389764761e3cf99af132f5c6764d5f21'
    Replit-Restored-To:edab637d389764761e3cf99af132f5c6764d5f21

--- client/src/pages/SheetMusic.tsx
+++ client/src/pages/SheetMusic.tsx
@@ -3,10 +3,10 @@ import { Link } from "wouter";
 
 const SheetMusic: FC = () => {
   return (
-    <div className="min-h-screen w-full bg-white p-8">
+    <div className="min-h-screen w-full bg-white p-8 relative">
       {/* Tutorial Link */}
       <Link href="/" className="absolute top-4 left-4 text-gray-600 hover:text-gray-900 transition-colors">
-        ← back
+        ← tutorial
       </Link>
 
       <div className="max-w-4xl mx-auto space-y-12">
@@ -21,34 +21,27 @@ const SheetMusic: FC = () => {
           ></iframe>
         </div>
 
-        {/* Sheet Music Content */}
-        <div className="font-mono text-center whitespace-pre-wrap text-gray-900 leading-relaxed">
-{`                              Em                   
-And I don't want the world to see me
-                    Gmaj                     
-'Cause I don't think that they'd understand
-                   Em                         A
-When everything's made to be broken
-                 C                 D          Em
-I just want you to know who I am
-
-                     Em
-And you can't fight the tears that ain't coming
-                    Gmaj
-Or the moment of truth in your lies
-                   Em                         A
-When everything feels like the movies
-                  C                D          Em
-Yeah you bleed just to know you're alive
-
-                              Em                   
-And I don't want the world to see me
-                    Gmaj                     
-'Cause I don't think that they'd understand
-                   Em                         A
-When everything's made to be broken
-                 C                 D          Em
-I just want you to know who I am`}
+        {/* PDF Viewer */}
+        <div className="space-y-8">
+          <object
+            data="/sheet-music.pdf#page=1"
+            type="application/pdf"
+            className="w-full h-[842px]"
+          >
+            <p>Unable to display PDF file. <a href="/sheet-music.pdf">Download</a> instead.</p>
+          </object>
+          <object
+            data="/sheet-music.pdf#page=2"
+            type="application/pdf"
+            className="w-full h-[842px]"
+          >
+          </object>
+          <object
+            data="/sheet-music.pdf#page=3"
+            type="application/pdf"
+            className="w-full h-[842px]"
+          >
+          </object>
         </div>
       </div>
     </div>
